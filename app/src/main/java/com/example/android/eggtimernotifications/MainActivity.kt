0e4ccda6bfc51ed3c137b0b5553644e613b19742
Accipiter7 <thygrace099@gmail.com>
2020-06-25 06:07:03 +0000

    clean up

--- app/src/main/java/com/example/android/eggtimernotifications/MainActivity.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MainActivity.kt
@@ -18,7 +18,6 @@ package com.example.android.eggtimernotifications
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android.eggtimernotifications.ui.EggTimerFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,10 +25,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, EggTimerFragment.newInstance())
-                .commitNow()
-        }
+       
     }
 }
